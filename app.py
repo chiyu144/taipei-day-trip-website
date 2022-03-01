88107450210902805f48a375f8e422e7214bff32
kyo144 <kwsmcy@gmail.com>
2022-03-02 03:19:49 +0800

    chore: part1-1 done
    1. 建立 2 個 Table，1 個放景點列表，1 個放景點圖片網址
    2. 自訂 column 命名規則並套用：全小寫、底線代替空白、不要有前綴、不要有阿拉伯數字
    3. 圖片網址副檔名過濾掉非 jpg、png 的，並將副檔名改為全小寫。

--- app.py
+++ app.py
@@ -1,20 +1,55 @@
-from flask import *
-app=Flask(__name__)
-app.config["JSON_AS_ASCII"]=False
-app.config["TEMPLATES_AUTO_RELOAD"]=True
+from flask import Flask, current_app, render_template
+import configparser
+from mysql.connector import Error, pooling
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def create_db_pool():
+  return pooling.MySQLConnectionPool(
+    pool_name = config['Mysql']['pool_name'],
+    pool_size = int(config['Mysql']['pool_size']),
+    host = config['Mysql']['host'],
+    port = int(config['Mysql']['port']),
+    user = config['Mysql']['user'],
+    password = config['Mysql']['password'],
+    database = config['Mysql']['database']
+  )
+
+def db_cnx():
+  try:
+    cnx = current_app.db_pool.get_connection()
+    if cnx.is_connected():
+      return cnx
+  except Error as e:
+    print('MySql Connection Pool error: ', e)
+
+app = Flask(__name__)
+with app.app_context():
+  current_app.db_pool = create_db_pool()
+  current_app.db_cnx = db_cnx
+
+app.config['ENV'] = config['App']['env']
+app.config['JSON_AS_ASCII'] = config['App'].getboolean('json_as_ascii')
+app.config["TEMPLATES_AUTO_RELOAD"] = config['App'].getboolean('templates_auto_reload')
 
 # Pages
 @app.route("/")
 def index():
-	return render_template("index.html")
+  return render_template("index.html")
 @app.route("/attraction/<id>")
 def attraction(id):
-	return render_template("attraction.html")
+  return render_template("attraction.html")
 @app.route("/booking")
 def booking():
-	return render_template("booking.html")
+  return render_template("booking.html")
 @app.route("/thankyou")
 def thankyou():
-	return render_template("thankyou.html")
+  return render_template("thankyou.html")
 
-app.run(port=3000)
+@app.route("/attractions")
+def test():
+  return "YOO!!"
+
+if __name__ == "__main__":
+  app.run(port=3000, debug=True)
