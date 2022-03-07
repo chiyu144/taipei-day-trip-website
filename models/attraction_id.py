69ddcaf88c73bf6fab6bac2e0c3ed9adcd188556
kyo144 <kwsmcy@gmail.com>
2022-03-07 16:07:05 +0800

    update: 優化 attractions、attraction_id 2 個 API

--- models/attraction_id.py
+++ models/attraction_id.py
@@ -9,26 +9,25 @@ bp_m_attraction_id = Blueprint('m_attraction_id', __name__)
 @with_cnx(need_commit = False)
 def query_attraction_id(cursor, attraction_id):
   query_sql = """
-    select attractions.id, attractions.name, attractions.category, attractions.description, attractions.address, 
-    attractions.transport, attractions.mrt, attractions.latitude, attractions.longitude, group_concat(attractions_imgs.img_url) as images 
-    from attractions_imgs inner join attractions on attractions.id = attractions_imgs.attraction_id 
-    where attraction_id = %s
-    group by attraction_id
+    SELECT attractions.id, attractions.name, attractions.category, attractions.description, attractions.address, 
+    attractions.transport, attractions.mrt, attractions.latitude, attractions.longitude, group_concat(attractions_imgs.img_url) AS images 
+    FROM attractions_imgs INNER JOIN attractions ON attractions.id = attractions_imgs.attraction_id 
+    WHERE attraction_id = %s
+    GROUP BY attraction_id
     """
   cursor.execute(query_sql, (attraction_id, ))
-  attraction = cursor.fetchone()
+  cols = [col[0] for col in cursor.description]
+  attraction = [dict(zip(cols, row)) for row in cursor.fetchone()]
   return attraction
 
 @bp_m_attraction_id.route("/attraction/<int:attraction_id>", methods=["GET"])
 def api_attraction_id(attraction_id):
   try:
-    cols = ["id", "name", "category", "description", "address", "transport", "mrt", "latitude", "longitude", "images"]
-    value = query_attraction_id(attraction_id)
-    result = dict(zip(cols, value))
+    result = query_attraction_id(attraction_id)
     result["images"] = result["images"].split(",")
     return jsonify({"data": result})
   except Exception as e:
-    if value is None:
+    if result is None:
       abort(400, description="Parameter attraction id is incorrect.")
     else:
       abort(500, description=abort_msg(e))
--- models/attractions.py
+++ models/attractions.py
@@ -8,23 +8,18 @@ bp_m_attractions = Blueprint('m_attractions', __name__)
 
 @with_cnx(need_commit = False)
 def query_attractions(cursor, page_unit, page, keyword):
-  start_index = 0 if page == 0 else page * page_unit
-  if keyword:
-    cursor.execute("""
-      select attractions.id, attractions.name, attractions.category, attractions.description, attractions.address, 
-      attractions.transport, attractions.mrt, attractions.latitude, attractions.longitude, group_concat(attractions_imgs.img_url) as images 
-      from attractions_imgs inner join attractions on attractions.id = attractions_imgs.attraction_id 
-      where name like %s
-      group by attraction_id limit %s, %s
-      """, ("%"+keyword+"%", start_index, page_unit))
-  else:
-    cursor.execute("""
-      select attractions.id, attractions.name, attractions.category, attractions.description, attractions.address, 
-      attractions.transport, attractions.mrt, attractions.latitude, attractions.longitude, group_concat(attractions_imgs.img_url) as images 
-      from attractions_imgs inner join attractions on attractions.id = attractions_imgs.attraction_id 
-      group by attraction_id limit %s, %s
-      """, (start_index, page_unit))
-  attractions = cursor.fetchall()
+  start_index = page * page_unit
+  keyword_val = keyword if keyword else '%'
+  query_sql = """
+    SELECT attractions.id, attractions.name, attractions.category, attractions.description, attractions.address, 
+    attractions.transport, attractions.mrt, attractions.latitude, attractions.longitude, group_concat(attractions_imgs.img_url) AS images 
+    FROM attractions_imgs INNER JOIN attractions ON attractions.id = attractions_imgs.attraction_id 
+    WHERE name LIKE %s
+    GROUP BY attraction_id LIMIT %s, %s
+  """
+  cursor.execute(query_sql, ("%"+keyword_val+"%", start_index, page_unit))
+  cols = [col[0] for col in cursor.description]
+  attractions = [dict(zip(cols, row)) for row in cursor.fetchall()]
   return attractions
 
 @bp_m_attractions.route("/attractions", methods=["GET"])
@@ -36,9 +31,7 @@ def api_attractions():
     if page is None:
       abort(400, description="Parameter page <'int'> is required.")
     else:
-      cols = ["id", "name", "category", "description", "address", "transport", "mrt", "latitude", "longitude", "images"]
-      values = query_attractions(page_unit, page, keyword)
-      result = [dict(zip(cols, value)) for value in values]
+      result = query_attractions(page_unit, page, keyword)
       for index, row in enumerate(result):
         result[index]["images"] = row["images"].split(",")
       next_page = None if len(result) < page_unit else page + 1
