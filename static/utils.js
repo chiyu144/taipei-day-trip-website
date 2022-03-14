cfd8064c1d91e6dd5fe3c0d2cf747cd71c295ae8
kyo144 <kwsmcy@gmail.com>
2022-03-14 18:57:12 +0800

    ui: 自製 carousel 實裝 90%

--- static/utils.js
+++ static/utils.js
@@ -24,25 +24,22 @@ export class Carousel {
     this.wrapCarousel = document.querySelector(`#${id}`);
     this.carousel = this.wrapCarousel.querySelector('.carousel');
     this.slides = this.carousel.querySelectorAll('.slide');
+    this.arrowButtons = this.wrapCarousel.querySelectorAll('.arrow');
+    this.indicatorButtons = this.wrapCarousel.querySelectorAll('.indicator');
     this.carouselLength = this.slides.length;
     this.currentSlideNum = 1;
     this.currentCarouselPos = -100;
     this.direction = undefined;
     this.isAnimating = false;
   }
-
   init() {
+    this.carousel.style.left = '-100%';
     this.listenEvents();
     this.cloneFirstAndLastSlide();
   }
-
   listenEvents() {
-    console.log('listenEvent', this);
-    const arrowButtons = this.wrapCarousel.querySelectorAll('.arrow');
-    const indicatorButtons = this.wrapCarousel.querySelectorAll('.indicator');
-    arrowButtons.forEach(arrowButton => {
+    this.arrowButtons.forEach(arrowButton => {
       arrowButton.addEventListener('click', e => {
-        console.log('click carousel arrow button', this);
         this.direction = parseInt(e.currentTarget.getAttribute('data-direction'));
         if (!this.isAnimating) { 
           this.currentSlideNum += this.direction;
@@ -50,17 +47,15 @@ export class Carousel {
         };
       });
     });
-    indicatorButtons.forEach((indicatorButton, targetSlideIndex) => {
-      indicatorButton.addEventListener('click', e => {
-        const targetSlide = this.slides[targetSlideIndex];
+    this.indicatorButtons.forEach((indicatorButton, targetSlideIndex) => {
+      indicatorButton.addEventListener('click', () => {
         if (this.currentSlideNum !== targetSlideIndex + 1 && !this.isAnimating) {
-          direction = currentSlideNum - 1 > targetSlideIndex ? -1 : 1;
-          slide(targetSlideIndex);
+          this.direction = this.currentSlideNum - 1 > targetSlideIndex ? -1 : 1;
+          this.slide(targetSlideIndex);
         };
       })
     })
   }
-
   cloneFirstAndLastSlide() {
     const firstSlide = this.slides[0];
     const firstSlideClone = firstSlide.cloneNode(true);
@@ -70,13 +65,43 @@ export class Carousel {
     this.carousel.appendChild(firstSlideClone);
     this.carousel.insertBefore(lastSlideClone, firstSlide);
   };
-
-  clickArrowButton() {
-
+  animateIndicator(targetSlideIndex) {
+    const activeIndicator = this.wrapCarousel.querySelector('.active-indicator');
+    if(activeIndicator) {
+      activeIndicator.classList.remove('active-indicator');
+    };
+    this.indicatorButtons[targetSlideIndex].classList.add('active-indicator');
+  }
+  animateSlide(newCarouselPos, isOnClone, targetSlideIndex) {
+    const start = new Date();
+    const timerId = setInterval(() => {
+      const timePassed = new Date - start;
+      let progress = timePassed / 500;
+      if (progress > 1) { progress = 1; }
+      let delta = Math.pow(progress, 2);
+      this.carousel.style.left = `${this.currentCarouselPos + Math.abs(newCarouselPos - this.currentCarouselPos) * delta * this.direction * -1}%`;
+      if( progress === 1 ) {
+        clearInterval(timerId);
+        this.isAnimating = false;
+        this.currentCarouselPos = newCarouselPos;
+        if (isOnClone) {
+          this.currentSlideNum = this.currentSlideNum < 1 ? this.carouselLength : 1;
+          this.currentCarouselPos = this.currentSlideNum * 100 * -1;
+          this.carousel.style.left = `${this.currentCarouselPos}%`;
+        } else {
+          this.currentSlideNum = targetSlideIndex !== null ? targetSlideIndex + 1 : this.currentSlideNum;
+          this.currentCarouselPos = this.currentSlideNum * 100 * -1;
+        }
+        this.animateIndicator(targetSlideIndex !== null ? targetSlideIndex : this.currentSlideNum - 1);
+      }
+    }, 20);
+  }
+  slide(targetSlideIndex) {
+    const isOnClone = this.currentSlideNum < 1 || this.currentSlideNum > this.carouselLength;
+    const newCarouselPos = isOnClone
+    ? this.direction > 0 ? (this.carouselLength + 1) * 100 * -1 : 0
+    : (targetSlideIndex !== null ? targetSlideIndex + 1 : this.currentSlideNum) * 100 * -1;
+    this.isAnimating = true;
+    this.animateSlide(newCarouselPos, isOnClone, targetSlideIndex);
   };
-
-  clickIndicator() {
-
-  };
-
 }
