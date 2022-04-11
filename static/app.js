805184cc0f2415b19639b32fffaa9cf514a3bdcc
kyo144 <kwsmcy@gmail.com>
2022-04-12 06:38:30 +0800

    feature: restructure app.js and add input validation logic

--- static/app.js
+++ static/app.js
@@ -1,29 +1,33 @@
 import { userApi } from './apis.js'
-import { checkUserState } from './utils.js'
+import { checkUserState, inputValidation, checkClassExist, clearInputInvalidStyle } from './utils.js'
 
-const showMsgAuth = (msg) => {
-  const msgAuth = document.querySelector('.msg-auth');
-  !msgAuth.classList.contains('sentinel-auth') && msgAuth.classList.add('sentinel-auth');
-  msgAuth.textContent = `${msg}`
+let isLogin = false;
+
+const showAuthMsg = (msg) => {
+  const authMsg = document.querySelector('.msg-auth');
+  !authMsg.classList.contains('sentinel-auth') && authMsg.classList.add('sentinel-auth');
+  authMsg.textContent = `${msg}`
 };
-const clearMsgAuth = () => {
-  const msgAuth = document.querySelector('.msg-auth');
-  msgAuth.textContent = '';
-  msgAuth.classList.contains('sentinel-auth') && msgAuth.classList.remove('sentinel-auth');
+const clearAuthMsg = () => {
+  const authMsg = document.querySelector('.msg-auth');
+  authMsg.textContent = '';
+  authMsg.classList.contains('sentinel-auth') && authMsg.classList.remove('sentinel-auth');
 };
 
-export const getUser = async(triggerAuth, bookingNum) => {
+export const getUser = async(authModalTrigger, bookingNum) => {
   const member = await checkUserState();
   if (member) {
-    const userState = await userApi('GET');
-    triggerAuth.textContent = '登出系統';
-    bookingNum.textContent = userState.data.booking_num;
+    authModalTrigger.textContent = '登出系統';
+    bookingNum.textContent = member.booking_num;
     bookingNum.style.visibility = 'visible';
-    sessionStorage.setItem('member', JSON.stringify(userState.data));
+    sessionStorage.setItem('member', JSON.stringify(member));
+    isLogin = true;
   } else {
-    triggerAuth.textContent = '登入/註冊';
+    if (window.location.pathname === '/booking') { window.location.href = '/' } 
+    authModalTrigger.textContent = '登入/註冊';
     bookingNum.style.visibility = 'hidden';
     sessionStorage.setItem('member', '');
+    isLogin = false;
   }
 };
 export const postUser = async({ userEmail, userPassword, userName }) => {
@@ -32,8 +36,8 @@ export const postUser = async({ userEmail, userPassword, userName }) => {
     email: userEmail,
     password: userPassword
   });
-  if (res?.ok) { showMsgAuth('註冊成功，請重新登入'); };
-  if (res?.error) { showMsgAuth(res.message); }
+  if (res?.ok) { showAuthMsg('註冊成功，請重新登入'); };
+  if (res?.error) { showAuthMsg(res.message); }
 };
 export const patchUser = async({ userEmail, userPassword }) => {
   const res = await userApi('PATCH', {
@@ -41,7 +45,7 @@ export const patchUser = async({ userEmail, userPassword }) => {
     password: userPassword
   });
   if (res?.ok) { window.location.reload(); };
-  if (res?.error) { showMsgAuth(res.message); };
+  if (res?.error) { showAuthMsg(res.message); };
 };
 export const deleteUser = async() => {
   const res = await userApi('DELETE');
@@ -50,73 +54,85 @@ export const deleteUser = async() => {
 
 
 window.addEventListener('load', async() => {
-  const navBooking = document.querySelector('#nav-booking');
-  const formAuth = document.querySelector('#form-auth');
-  const titleAuth = formAuth.querySelector('div:first-child');
-  const fieldUserName = document.querySelector('.field-user-name');
-  const buttonAuth = formAuth.querySelector('button');
-  const msgToggleAuth = document.querySelector('#toggle-auth > span');
-  const toggleAuth = document.querySelector('#toggle-auth > a');
-  const triggerAuth = document.querySelector('#trigger-auth');
-  const triggers = document.querySelectorAll('[data-modal]');
+  const bookingNav = document.querySelector('#nav-booking');
+  const authForm = document.querySelector('#form-auth');
+  const authFormTitle = authForm.querySelector('div:first-child');
+  const userNameField = document.querySelector('.field-user-name');
+  const authInputs = document.querySelectorAll('#form-auth input');
+  const authButton = authForm.querySelector('button');
+  const authMsgToggle = document.querySelector('#toggle-auth > span');
+  const authToggle = document.querySelector('#toggle-auth > a');
+  const authModalTrigger = document.querySelector('#trigger-auth');
+  const modalTriggers = document.querySelectorAll('[data-modal]');
   const bookingNum = document.querySelector('#nav-booking-num');
   
-  await getUser(triggerAuth, bookingNum);
+  await getUser(authModalTrigger, bookingNum);
 
-  navBooking.addEventListener('click', async(e) => {
+  bookingNav.addEventListener('click', async(e) => {
     e.preventDefault();
-    if(await checkUserState()) {
+    if(sessionStorage.getItem('member') !== '') {
       window.location.href = '/booking';
     } else {
-      triggerAuth.click();
+      authModalTrigger.click();
     }
   });
   
-  triggers.forEach(trigger => {
-    trigger.addEventListener('click', e => {
+  modalTriggers.forEach(modalTrigger => {
+    modalTrigger.addEventListener('click', e => {
       e.preventDefault();
-      if (trigger.id === 'trigger-auth' && (trigger.textContent === '登出系統' || '')) { return; }; 
-      const modal = document.getElementById(trigger.dataset.modal);
+      if (modalTrigger.id === 'trigger-auth' && isLogin) { return; }; 
+      const modal = document.getElementById(modalTrigger.dataset.modal);
       modal.classList.add('open-modal');
-      const exits = modal.querySelectorAll('.exit-modal');
-      exits.forEach(exit => {
-        exit.addEventListener('click', e => {
+      const modalExits = modal.querySelectorAll('.exit-modal');
+      modalExits.forEach(modalExit => {
+        modalExit.addEventListener('click', e => {
           e.preventDefault();
           modal.classList.remove('open-modal');
-          clearMsgAuth();
-          formAuth.reset();
+          clearAuthMsg();
+          if (modalTrigger.id === 'trigger-auth') {
+            authForm.reset();
+            const invalidElements = authForm.querySelectorAll('.input-invalid');
+            clearInputInvalidStyle(invalidElements);
+          };
         });
       });
     });
   });
 
-  triggerAuth.addEventListener('click', async(e) => {
+  authModalTrigger.addEventListener('click', async(e) => {
     e.preventDefault();
-    if (triggerAuth.textContent === '登出系統') {
-      await deleteUser();
-    };
+    isLogin && await deleteUser();
   });
 
-  toggleAuth?.addEventListener('click', e => {
+  authToggle?.addEventListener('click', e => {
     e.preventDefault();
-    titleAuth.textContent = titleAuth.textContent === '登入會員帳號' ? '註冊會員帳號' : '登入會員帳號';
-    msgToggleAuth.textContent = msgToggleAuth.textContent === '還沒有帳戶？' ? '已有帳戶？' : '還沒有帳戶？';
-    toggleAuth.textContent = toggleAuth.textContent === '點此註冊' ? '點此登入' : '點此註冊';
-    fieldUserName.classList.toggle('anime-field-user-name');
-    buttonAuth.textContent = buttonAuth.textContent === '登入帳戶' ? '註冊帳戶' : '登入帳戶';
-    clearMsgAuth();
-    formAuth.reset();
+    authFormTitle.textContent = authFormTitle.textContent === '登入會員帳號' ? '註冊會員帳號' : '登入會員帳號';
+    authMsgToggle.textContent = authMsgToggle.textContent === '還沒有帳戶？' ? '已有帳戶？' : '還沒有帳戶？';
+    authToggle.textContent = authToggle.textContent === '點此註冊' ? '點此登入' : '點此註冊';
+    userNameField.classList.toggle('anime-field-user-name');
+    authButton.textContent = authButton.textContent === '登入帳戶' ? '註冊帳戶' : '登入帳戶';
+    clearAuthMsg();
+    const invalidElements = authForm.querySelectorAll('.input-invalid');
+    invalidElements.length > 0 && clearInputInvalidStyle(invalidElements);
+    authForm.reset();
   });
 
-  formAuth.addEventListener('submit', async(e) => {
+  authInputs.addEventListener('keyup', e => {
+    checkClassExist(e.currentTarget, 'input-invalid') && e.currentTarget.classList.remove('input-invalid');
+    checkClassExist(e.currentTarget.nextElementSibling, 'input-icon-invalid') && e.currentTarget.nextElementSibling.classList.remove('input-icon-invalid');
+  });
+  authInputs.addEventListener('blur', e => inputValidation('email', e.currentTarget, e.currentTarget.value));
+
+  authForm.addEventListener('submit', async(e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const userEmail =  formData.get('user-email');
     const userPassword = formData.get('user-password');
     const userName = formData.get('user-name');
-    if (buttonAuth.textContent === '登入帳戶') {
+
+    if (authButton.textContent === '登入帳戶') {
       await patchUser({ userEmail, userPassword });
-    } else if (buttonAuth.textContent === '註冊帳戶') {
+    } else if (authButton.textContent === '註冊帳戶') {
       await postUser({ userEmail, userPassword, userName });
     };
   });
